e73598cc7a80789d6391f613ef84968bde8c4abd
Jacky Zhao <j.zhao2k19@gmail.com>
2022-06-10 14:09:39 -0700

    add scaffolding for other pages

--- components/Header.js
+++ components/Header.js
@@ -3,14 +3,14 @@ function capitalizeFirstLetter(string) {
 }
 
 export default function Header({ title }) {
-  const title_class = "text-4xl font-title mt-16 mb-16 md:mb-20 md:text-7xl font-semibold text-gray-primary";
-  const lower_class = "text-2xl block font-light italic text-xl md:text-4xl text-gray-primary";
+  const title_class = "flex flex-wrap justify-center gap-x-2 inline-block text-4xl font-title mt-16 mb-16 md:mb-20 md:text-7xl font-semibold text-gray-primary";
+  const lower_class = "inline-block font-light italic -mb-5 md:-mb-1 mt-1 md:mt-3 text-xl md:text-3xl text-gray-primary";
   const concat = title
     .split(" ")
     .map(word => {
       if (capitalizeFirstLetter(word) === word) {
         // title case
-        return word
+        return <span>{word}</span>
       } else {
         // regular
         return <span className={`${lower_class}`}>{word}</span>
--- components/PageLayout.js
+++ components/PageLayout.js
@@ -6,7 +6,7 @@ export default function LayoutComponents({ children }) {
     <Head />
     <div className="w-full flex my-12">
       <div className="h-[100vh] flex-none w-20"><Sidebar /></div>
-      <div className="font-body text-xl w-2/5 mx-auto">
+      <div className="font-body text-xl w-2/5 mx-auto prose">
         {children}
       </div>
     </div>
