4e4f3f1458075d2517d4c884e796a1ca91ce611d
Jacky Zhao <j.zhao2k19@gmail.com>
2022-06-30 13:23:37 -0700

    mobile friendly

--- components/LoadingIframe.js
+++ components/LoadingIframe.js
@@ -2,7 +2,7 @@ import ScaleLoader from "react-spinners/ScaleLoader"
 import LoadingFrame from 'react-loading-iframe';
 
 const LoadingIframe = () => {
-  return <div style={{ "margin-left": "-25%", "width": "150%", "height": "700px", "border-radius": "5px", "position": "relative" }}>
+  return <div style={{ "width": "100%", "height": "700px", "border-radius": "5px", "position": "relative" }}>
     <LoadingFrame skeleton={<ScaleLoader color="#333" className="text-center mt-20" />
     } className="airtable-embed" src="https://airtable.com/embed/shrvSk01p3E1wwH77" frameborder="0" onmousewheel="" width="100%" height="100%"></LoadingFrame>
   </div>
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useState, useEffect } from "react"
+import { IoIosMenu } from "react-icons/io";
 
 const LINKS = [
   { label: "Introduction", href: "/" },
@@ -11,19 +13,36 @@ const LINKS = [
 
 export default function Sidebar() {
   const router = useRouter()
+  const [open, setOpen] = useState(true)
+
   const calculateBg = (href) => (
     router.pathname === href ||
     (router.pathname === "" && href === "/")
   ) ? 'bg-gray-hover' : 'bg-gray-wash'
 
-  return <div className="font-body text-xl ml-36 fixed -translate-y-1/2 top-1/2">
-    <h2 className="font-bold">Table of Contents</h2>
-    <ol className="list-[upper-roman] ml-4 my-4">
-      {LINKS.map((link, i) => <li key={i} className="my-2">
-        <Link href={link.href}>
-          <a className={`no-underline rounded-md ${calculateBg(link.href)} hover:bg-gray-hover transition ease-in-out px-1`}>{link.label}</a>
-        </Link>
-      </li>)}
-    </ol>
+  const handleResize = () => {
+    setOpen(window.innerWidth > 1280)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, [])
+
+  const bg = open ? "bg-gray-wash border-r border-gray" : ""
+
+  return <div className={`h-[100vh] z-[9999] flex flex-col fixed justify-center font-body text-xl px-36 xl:bg-transparent xl:border-none ${bg}`}>
+    <IoIosMenu className="absolute left-0 top-0 m-8 xl:hidden block cursor-pointer" size={40} onClick={() => setOpen(state => !state)} />
+    <div className={open ? "" : "hidden"}>
+      <h2 className="font-bold">Table of Contents</h2>
+      <ol className="list-[upper-roman] ml-4 my-4">
+        {LINKS.map((link, i) => <li key={i} className="my-2">
+          <Link href={link.href}>
+            <a className={`no-underline rounded-md ${calculateBg(link.href)} hover:bg-gray-hover transition ease-in-out px-1`}>{link.label}</a>
+          </Link>
+        </li>)}
+      </ol>
+    </div>
   </div>
 }
