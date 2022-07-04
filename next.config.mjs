28d673a6c0d5749929eee501f3a573829c51fa82
Jacky Zhao <j.zhao2k19@gmail.com>
2022-07-04 15:22:54 -0700

    jump to header

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
-const withMDX = require('@next/mdx')({
+import mdx from '@next/mdx'
+import rehypeSlug from 'rehype-slug'
+
+const withMDX = mdx({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [],
-    rehypePlugins: [],
+    rehypePlugins: [rehypeSlug],
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
   },
 })
-module.exports = withMDX({
+
+export default withMDX({
   // Append the default value with md extensions
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 })
