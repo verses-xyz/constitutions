4cd6aa07a4ec5f42a05cc3bc03cf593f1f76774a
yichabod <maxlangenkamp@me.com>
2022-07-08 18:55:39 -0400

    reordering table of contents

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,9 +7,10 @@ import useOutsideClick from "../hooks/useOutsideClick"
 const LINKS = [
   { label: "Introduction", href: "/" },
   { label: "Analysis", href: "/article" },
+  { label: "Constitutions", href: "https://github.com/metagov/constitution-template/tree/main/constitutions"},
   { label: "Guide", href: "/guide" },
   { label: "Template", href: "/template" },
-  { label: "Dataset", href: "https://airtable.com/shr5r9t19gfW7YYPs/tbltBIhUFqZE1tfle"}
+  
 ]
 
 export default function Sidebar() {
