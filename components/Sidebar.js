92cad50e38da4be14369826fc7cfed274aaf69e0
Jacky Zhao <j.zhao2k19@gmail.com>
2022-07-07 19:00:58 -0700

    fix footnote overlap

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,8 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import { IoIosMenu } from "react-icons/io";
+import useOutsideClick from "../hooks/useOutsideClick"
 
 const LINKS = [
   { label: "Introduction", href: "/" },
@@ -13,6 +14,13 @@ const LINKS = [
 export default function Sidebar() {
   const router = useRouter()
   const [open, setOpen] = useState(true)
+  const ref = useRef(null);
+
+  useOutsideClick(ref, () => {
+    if (open) {
+      setOpen(false);
+    }
+  });
 
   const calculateBg = (href) => (
     router.pathname === href ||
@@ -31,10 +39,9 @@ export default function Sidebar() {
 
   const bg = open ? "bg-gray-wash border-r border-gray" : ""
 
-  console.log(open)
   return <div className={`h-[100vh] z-[9999] flex flex-col fixed justify-center font-body text-xl xl:bg-transparent xl:border-none ${bg}`}>
     <IoIosMenu className="absolute left-0 top-0 m-8 xl:hidden block cursor-pointer" size={40} onClick={() => setOpen(state => !state)} />
-    <div className={open ? "px-32" : "hidden pointer-events-none"}>
+    <div className={open ? "px-32" : "hidden pointer-events-none"} ref={ref}>
       <h2 className="font-bold">Table of Contents</h2>
       <ol className="list-[upper-roman] ml-4 my-4">
         {LINKS.map((link, i) => <li key={i} className="my-2">
