a643a0269511d6c0e05752d916590f27be6cf4f6
Jacky Zhao <j.zhao2k19@gmail.com>
2022-07-07 20:44:50 -0700

    fix footnotes on guide

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -6,7 +6,7 @@ export default function LayoutComponents({ children }) {
     <Head />
     <div className="w-full flex">
       <Sidebar />
-      <div className="font-body my-24 text-xl xl:w-2/5 mx-auto prose w-4/5">
+      <div className="font-body relative my-24 text-xl xl:w-2/5 mx-auto prose w-4/5">
         {children}
       </div>
     </div>
