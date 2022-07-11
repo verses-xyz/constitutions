ed410b8a80b193e8610d55cb5d8ae66375dbc81e
Jacky Zhao <j.zhao2k19@gmail.com>
2022-07-11 14:57:03 -0700

    fix mobile footnote word break bug

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -6,7 +6,7 @@ export default function LayoutComponents({ children }) {
     <Head />
     <div className="w-full flex">
       <Sidebar />
-      <div className="font-body relative my-24 text-xl xl:w-2/5 mx-auto prose w-4/5">
+      <div className="font-body relative my-24 text-xl xl:w-2/5 mx-auto prose w-4/5 break-words">
         {children}
       </div>
     </div>
